1d3daf604731cec4ce74ba59f41454c6d6102ef3
variaschaefer <varvara.schaefer@fau.de>
2021-10-08 13:37:42 +0200

    added server stuff for annotations

--- formulaPos/app.py
+++ formulaPos/app.py
@@ -5,6 +5,12 @@ app = Flask(__name__)
 
 @app.route('/getAnnotations/<doc_id>')
 def getAnnot(doc_id):
-    with open(f'{doc_id}.json') as f:
+    with open(f'annotations/{doc_id}.json') as f:
         return f.read()
 
+@app.route('/storeAnnotations/<doc_id>')
+def storeAnnot(doc_id):
+    data = request.get_data(as_text=True)
+    with open(f'annotations/{doc_id}.json','w') as f:
+        f.write(data)
+    return ''
