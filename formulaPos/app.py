e94f8609b3b875160d67e2275e7701c9294f40b7
variaschaefer <varvara.schaefer@fau.de>
2021-10-08 13:59:11 +0200

    added java script annotations to server

--- formulaPos/app.py
+++ formulaPos/app.py
@@ -13,4 +13,14 @@ def storeAnnot(doc_id):
     data = request.get_data(as_text=True)
     with open(f'annotations/{doc_id}.json','w') as f:
         f.write(data)
-    return ''
+    return ''  # because the server complains if nothing is returned
+
+@app.route('/document/<doc_id>')
+def document(doc_id):
+    with open('modify.js') as m:
+        js = m.read()
+    with open(f'data/{doc_id}.html') as f:
+        s = f.read()
+        new_string = f'<script>\n {js} \n </script> \n </html>'
+        s = s.replace('</html>', new_string)
+    return s
