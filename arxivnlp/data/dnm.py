4b00c8b9698ed7559e2928dcea848b158f58525e
variaschaefer <varvara.schaefer@fau.de>
2021-11-30 16:05:25 +0100

    added StringToken

--- arxivnlp/data/dnm.py
+++ arxivnlp/data/dnm.py
@@ -1,4 +1,4 @@
-from typing import Set
+from typing import Set, List
 from lxml import etree
 
 
@@ -7,7 +7,7 @@ class DnmConfig(object):
         self.nodes_to_skip = nodes_to_skip
         self.classes_to_skip = classes_to_skip
 
-    def skip_node(self, node) -> bool:
+    def skip_node(self, node: etree.Element) -> bool:
         classes_of_node = node.get('class')
         if classes_of_node is not None:
             classes_of_node = classes_of_node.split()
@@ -19,17 +19,38 @@ class DnmConfig(object):
         return False
 
 
+class StringToken(object):
+    def __init__(self, content: str, backref_node: etree.Element, backref_type: str):
+        self.content = content
+        self.backref_node = backref_node
+        self.backref_type = backref_type
+
+    def get_string(self) -> str:
+        return self.content
+
+    def insert_node(self, node: etree.Element, pos: int):
+        if self.backref_type == 'text':
+            self.backref_node.insert(0, node)  # 0 means child no. 0
+            node.tail = self.backref_node.text[pos:]
+            self.backref_node.text = self.backref_node.text[:pos]
+        elif self.backref_type == 'tail':
+            self.backref_node.addnext(node)     # add a new sibling and move tail behind
+            self.backref_node.tail = node.tail[:pos]
+            node.tail = node.tail[pos:]
+        else:
+            raise Exception(f'Unsupported backref type {self.backref_type}')
+
+
 class Dnm(object):
     def __init__(self, tree: etree.ElementTree, dnm_config: DnmConfig):
         self.tree = tree
-        self.stream = []
+        self.tokens: List[StringToken] = []
         self.dnm_config = dnm_config
         self.append_to_stream(tree.getroot())
 
-
-    def append_to_stream(self, node):
+    def append_to_stream(self, node: etree.Element):
         if not self.dnm_config.skip_node(node):
-            self.stream.append(node.text)
+            self.tokens.append(StringToken(content=node.text, backref_node=node, backref_type='text'))
             for child in node:
                 self.append_to_stream(child)
-                self.stream.append(child.tail)
+                self.tokens.append(StringToken(content=child.tail, backref_node=child, backref_type='tail'))
--- arxivnlp/test/test_dnm.py
+++ arxivnlp/test/test_dnm.py
@@ -2,10 +2,25 @@ import unittest
 from arxivnlp.data.dnm import Dnm, DnmConfig
 from lxml import etree
 import os
+import io
+
 
 class TestDnm(unittest.TestCase):
     def test_simple(self):
         source = os.path.join(os.path.dirname(__file__),'resources','test_simple.html')
         tree = etree.parse(source)
         doc = Dnm(tree, dnm_config=DnmConfig(nodes_to_skip={'head'}, classes_to_skip={'ltx_para'}))
-        print(doc.stream)
+        print(doc.tokens)
+        doc.tokens[3].insert_node(node=etree.XML('<span> hello world </span>'),pos=3)
+        newHTML = etree.tostring(tree.getroot())
+        print(' ')
+        print(newHTML)
+
+    def test_insert(self):
+        html = '<a>abc<b>hgj</b>nope<c></c></a>'
+        tree = etree.parse(io.StringIO(html))
+        dnm = Dnm(tree, dnm_config=DnmConfig(nodes_to_skip=set(), classes_to_skip=set()))
+        dnm.tokens[0].insert_node(node=etree.XML('<d></d>'),pos=2)
+        dnm.tokens[2].insert_node(node=etree.XML('<e></e>'),pos=2)
+        newHTML = etree.tostring(tree.getroot())
+        self.assertEqual(newHTML, b'<a>ab<d/>c<b>hgj</b>no<e/>pe<c/></a>')
