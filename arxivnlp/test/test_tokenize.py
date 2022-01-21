e8bd7c8aa6f6b93f2f2ce6e2c46098fdfd51bca7
variaschaefer <varvara.schaefer@fau.de>
2022-01-21 18:36:32 +0100

    tried nltk part of speach tags

--- arxivnlp/test/test_tokenize.py
+++ arxivnlp/test/test_tokenize.py
@@ -7,6 +7,8 @@ from arxivnlp.data.dnm import Dnm, DEFAULT_DNM_CONFIG
 from arxivnlp.sentence_tokenize import sentence_tokenize
 from arxivnlp.word_tokenize import word_tokenize
 
+import nltk
+
 
 class TestDnm(unittest.TestCase):
     def test_tokenize(self):
@@ -24,8 +26,13 @@ class TestDnm(unittest.TestCase):
 
         for s in sentences[::-1]:
             doc.insert_node(node=etree.XML('<span style="color:red; font-size:150%">]</span>'), pos=s.backrefs[-1] + 1)
-            print(word_tokenize(s))
-            for word in word_tokenize(s)[::-1]:
+            words = word_tokenize(s)
+            string_list = [word.string for word in words]
+            tags = [pair[1] for pair in nltk.pos_tag(string_list)]
+            tagged_words = list(zip(words, tags))
+            for word, tag in tagged_words[::-1]:
+                doc.insert_node(node=etree.XML(f'<sup style="color:blue">{tag}</sup>'),
+                                pos=word.backrefs[-1] + 1)
                 doc.insert_node(node=etree.XML('<span style="color:orange; font-size:120%">]</span>'),
                                 pos=word.backrefs[-1] + 1)
                 doc.insert_node(node=etree.XML('<span style="color:orange; font-size:120%">[</span>'),
