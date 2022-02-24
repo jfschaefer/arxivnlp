9c2b4ee5af2aa52f292b0913afc3a2b582fbd353
jfschaefer <jan.frederik.schaefer@fau.de>
2022-02-24 13:29:00 +0100

    wikidata for units

---
+++ arxivnlp/examples/quantities/wikidata/query.py
@@ -0,0 +1,15 @@
+import requests
+
+
+def query(content: str) -> str:
+    result = requests.get('https://query.wikidata.org/sparql', params={'query': content}, headers={'Accept': 'text/csv'})
+    return result.text
+
+
+if __name__ == '__main__':
+    import sys
+    for file in sys.argv[1:]:
+        print(file)
+        with open(file) as fp1:
+            with open(file[:-6]+'.csv', 'w') as fp2:
+                fp2.write(query(fp1.read()))
